d99612f3dafc3215deb9ea05ae6c296487130be6
Pablo Arturo <pablocode010@gmail.com>
2023-11-21 18:35:50 -0600

    update home view

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ const router = Router();
 
 const renderHomePage = require("../controllers/homeController");
 
-router.get("/", renderHomePage)
+router.get("/", renderHomePage);
 
 module.exports = router;
