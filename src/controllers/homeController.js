0dd80063fe24fcfb3068276b29777f6178a9edde
Pablo Arturo <pablocode010@gmail.com>
2023-11-22 22:02:58 -0600

    update home view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,55 @@
 const renderHomePage = (req, res, next) => {
-    res.status(200).render("home");
+    const latestPosts = [
+        {
+            title: "Ciberseguridad en el siglo XXI: Protegiendo tu información",
+            imageSrc: "https://images.pexels.com/photos/325229/pexels-photo-325229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            synopsis: "Explora las últimas tendencias en ciberseguridad y aprende a proteger tus datos en la era digital."
+        },
+        {
+            title: "Desarrollo web moderno: Principios esenciales",
+            imageSrc: "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600",
+            synopsis: "Descubre los principios esenciales del desarrollo web moderno y cómo aplicarlos en tu próximo proyecto."
+        },
+        {
+            title: "Impacto de la inteligencia artificial en la medicina",
+            imageSrc: "https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            synopsis: "Descubre cómo la inteligencia artificial está revolucionando la industria de la salud y la medicina moderna."
+        }
+      ];
+
+      const recommendedPosts = [
+        {
+            title: "Ciberseguridad en el siglo XXI: Protegiendo tu información",
+            imageSrc: "https://images.pexels.com/photos/325229/pexels-photo-325229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            synopsis: "Explora las últimas tendencias en ciberseguridad y aprende a proteger tus datos en la era digital."
+        },
+        {
+            title: "Desarrollo web moderno: Principios esenciales",
+            imageSrc: "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600",
+            synopsis: "Descubre los principios esenciales del desarrollo web moderno y cómo aplicarlos en tu próximo proyecto."
+        },
+        {
+            title: "Impacto de la inteligencia artificial en la medicina",
+            imageSrc: "https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            synopsis: "Descubre cómo la inteligencia artificial está revolucionando la industria de la salud y la medicina moderna."
+        },
+        {
+            title: "Ciberseguridad en el siglo XXI: Protegiendo tu información",
+            imageSrc: "https://images.pexels.com/photos/325229/pexels-photo-325229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            synopsis: "Explora las últimas tendencias en ciberseguridad y aprende a proteger tus datos en la era digital."
+        },
+        {
+            title: "Desarrollo web moderno: Principios esenciales",
+            imageSrc: "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600",
+            synopsis: "Descubre los principios esenciales del desarrollo web moderno y cómo aplicarlos en tu próximo proyecto."
+        },
+        {
+            title: "Desarrollo web moderno: Principios esenciales",
+            imageSrc: "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600",
+            synopsis: "Descubre los principios esenciales del desarrollo web moderno y cómo aplicarlos en tu próximo proyecto."
+        }
+      ];
+    res.status(200).render("home", { latestPosts, recommendedPosts });
 }
 
 module.exports = renderHomePage;
